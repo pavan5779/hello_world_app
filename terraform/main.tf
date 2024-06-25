c41a7f24eded41813097197b3cb19f5457a34b74
pavan5779 <141653546+pavan5779@users.noreply.github.com>
2024-06-25 12:11:11 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,7 @@ terraform {
     region = "ap-south-1"
   }
 }
+
 provider "aws" {
   region = var.region
 }
@@ -75,24 +76,6 @@ resource "aws_ecs_service" "service" {
   }
 }
 
-resource "aws_security_group" "group" {
-  vpc_id = var.vpc_id
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_lb" "app_lb" {
   name               = "${var.app_name}-lb"
   internal           = false
@@ -102,10 +85,11 @@ resource "aws_lb" "app_lb" {
 }
 
 resource "aws_lb_target_group" "app_target_group" {
-  name     = "${var.app_name}-tg"
-  port     = 3000
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name        = "${var.app_name}-tg"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
 }
 
 resource "aws_lb_listener" "app_listener" {
